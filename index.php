dd314638d7989fb1c4e721b512af5276644d9998
Abraham Rivera-Garcia <arivera178@west-mec.org>
2020-02-07 03:02:28 -0700

    Commit 2.7.20

--- index.php
+++ index.php
@@ -2,22 +2,23 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>NMR Login</title><center>
-<link href="login.css" rel="stylesheet">
-</head>
-<img src="wmcct65.jpg" id="wmcct65logo">
-<body><div>
-<h1>
-	WMCC <br>Database Login
-</h1>
+<title>HOMEPAGE</title>
+<link href="index.css" rel="stylesheet">
+</head><center>
+<img src="wmcct65.jpg" id="wmcct65logo"></center><center><h1>
+	WMCC <br>Database
+</h1><div>
+<body><br><br>
+<h3>Items:</h3>
+    <button onclick="window.location.href = 'register.php';">Register as a Member!</button><br><br>
+    <button onclick="window.location.href = 'flexlogin.php';">Flex Conference</button><br><br>
+    <button onclick="window.location.href = 'regionalslogin.php';">Region 3 Conference</button><br><br>
+    <button onclick="window.location.href = 'statelogin.php';">State Leadership Conference</button><br><br>
+<h3>Due Dates:</h3>
+<p>Register as a Member: 08/30/20</p>
+<p>Flex Conference: 08/30/20</p>
+<p>Region 3 Conference: 08/30/20</p>
+<p>State Leadership Conference : 08/30/20</p>
 
-	<form name="login_form">
-		<label>Username: <br></label><br>
-		<input type="text" name="username" id="usernamefield"><br><br>
-		<label>Password: <br></label><br>
-		<input type="password" name="password" id="passwordfield"><br><br>
-		<input type="submit" value="login">
-	</form>
-    <br><a href="register.php">Register</a>
-</div></body></center>
+</div></center></body>
 </html>
---
+++ login_state.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>statelogin</title><center>
+<link href="index_style.css" rel="stylesheet">
+</head>
+<img src="wmcct65.jpg" id="wmcct65logo">
+<body><div>
+<h1>
+	WMCC <br>State Leadership Conference
+</h1>
+
+	<form name="login_form">
+		<label>Username: <br></label><br>
+		<input type="text" name="username" id="usernamefield"><br><br>
+		<label>Password: <br></label><br>
+		<input type="password" name="password" id="passwordfield"><br><br>
+		<input type="submit" value="login">
+	</form>
+    <br><a href="index.php">Home</a>
+</div></body></center>
+</html>
