fd1f38442266286ca6bb29ea96a99b2f3059010f
Zac <zacace@ingoglia.com>
2020-02-07 15:29:17 -0700

    0.00.02
    Added FB key
    Tried to auth the fiebase database

--- NodeJS/auth.js
+++ NodeJS/auth.js
@@ -1,2 +1,10 @@
-console.info("Hello World");
-console.log("SecondTest");
+// console.info("Hello World");
+// console.log("SecondTest");
+
+//Firebase serviceKey Auth
+var admin = require("firebase-admin");
+var serviceAccount = require("/key/serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://wmccloginauth.firebaseio.com"
+});
